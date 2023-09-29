50caf397c1967ea3f48a1d480e5c50ccff2b97c7
Shumon Khan <backlink.shumonkhan@gmail.com>
2023-09-30 02:04:49 +0600

    Wroking in chat app inbox

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const dotenv = require("dotenv");
 const mongoose = require("mongoose");
 const path = require("path");
 const cookieParser = require("cookie-parser");
+const moment = require("moment");
+const http = require("http");
 
 //Internal imports
 const { errorhandler, notFoundError } = require("./middlewares/common/errorHandler");
@@ -12,7 +14,18 @@ const usersRouter = require("./router/usersRouter");
 const inboxRouter = require("./router/inboxRouter");
 
 const app = express();
+const server = http.createServer(app);
+
+//.env file config
 dotenv.config();
+
+//socket.io setting. Here io is a clent io
+const io = require("socket.io")(server);
+//global.io here global is server global objeect. we can use io using global.io
+global.io = io;
+
+// set moment as app locals
+app.locals.moment = moment;
 
 //database connection
 mongoose
--- router/inboxRouter.js
+++ router/inboxRouter.js
@@ -1,13 +1,33 @@
-//External imports
+// external imports
 const express = require("express");
 
-//Internal imports
-const { getInbox } = require("../controller/inboxController");
+// internal imports
+const {
+    getInbox,
+    searchUser,
+    addConversation,
+    getMessages,
+    sendMessage,
+} = require("../controller/inboxController");
 const decoHtmlRes = require("../middlewares/common/decoHtmlRes");
 const { checkLogin } = require("../middlewares/common/checkLogin");
+const attachmentUpload = require("../middlewares/inbox/attachmentUpload");
 
 const router = express.Router();
 
+// inbox page
 router.get("/", decoHtmlRes("Inbox"), checkLogin, getInbox);
+
+// search user for conversation
+router.post("/search", checkLogin, searchUser);
+
+// add conversation
+router.post("/conversation", checkLogin, addConversation);
+
+// get messages of a conversation
+router.get("/messages/:conversation_id", checkLogin, getMessages);
+
+// send message
+router.post("/message", checkLogin, attachmentUpload, sendMessage);
 
 module.exports = router;
